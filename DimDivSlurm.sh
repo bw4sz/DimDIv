aed881852484404d956a2b25292925c66a2d9bca
bw4sz <benweinstein2010@gmail.com>
2013-10-17 11:31:56 -0400

    source files for cluster.

--- DimDivSlurm.sh
+++ DimDivSlurm.sh
@@ -3,9 +3,9 @@
 #SBATCH -J DimDiv2
 #SBATCH -o /home1/02443/bw4sz/DimDiv/DimDiv123.out
 #SBATCH -p normal
-#SBATCH -t 24:00:00
-#SBATCH -A TG-TRA120007   
-#SBATCH -n 100 # Total cores
+#SBATCH -t 48:00:00
+#SBATCH -A TG-DEB130023   
+#SBATCH -n 50 # Total cores
 
 #SBATCH --mail-user=benweinstein2010@gmail.com
 #SBATCH --mail-type=begin
@@ -16,14 +16,14 @@ module load R_mkl
 source /work/01125/yye00/ParallelR/sourceme.sh
 
 
-echo "Dim Div 1"
+#echo "Dim Div 1"
 
-ibrun RMPISNOW < /home1/02443/bw4sz/DimDiv/DimDivCluster3.R > /home1/02443/bw4sz/DimDiv/DimDiv1.out
+#ibrun RMPISNOW < /home1/02443/bw4sz/DimDiv/DimDivCluster1.R > /home1/02443/bw4sz/DimDiv/DimDiv1.out
 
 
 echo "Dim Div 2"
 
-ibrun RMPISNOW < /home1/02443/bw4sz/DimDiv/DimDivCluster3.R > /home1/02443/bw4sz/DimDiv/DimDiv2.out
+ibrun RMPISNOW < /home1/02443/bw4sz/DimDiv/DimDivCluster2.R > /home1/02443/bw4sz/DimDiv/DimDiv2.out
 
 
 
